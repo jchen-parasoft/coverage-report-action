008f0e4511a0839aa608371e186df11345487ba7
Jason <jchen@parasoft.com>
2024-03-12 11:16:27 +0800

    Update to become an action

--- generate_coverage.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem Cygwin: `cmd /c generate_coverage.bat`
-rem CMD: `generate-coverage.bat`
-rem Git Bash: `./generate-coverage.bat`
-rem PowerShell: `.\generate_coverage.bat`
-
-mvn process-test-classes jtest:agent test jtest:jtest "-Djtest.settings=./localsettings.properties" "-Djtest.config=builtin://Unit Tests" "-Djtest.report=./build/report/jtest/junit"
-
