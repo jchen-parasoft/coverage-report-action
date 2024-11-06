ce5d1060352d41321e063409d1f5f380a74a53d1
Jason <jchen@parasoft.com>
2024-11-06 17:56:15 +0800

    Update code

--- dist/index.js
+++ dist/index.js
@@ -8,17 +8,26 @@
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.TestsRunner = void 0;
+const child_process_1 = __nccwpck_require__(2081);
 const fs = __nccwpck_require__(7147);
+const core = __nccwpck_require__(2186);
 class TestsRunner {
     async generateSummaryTable(runOptions, reportData) {
         try {
             let markdownTable = '| File | Covered | Total | Percentage |\n';
-            markdownTable += '| -- | ---- | ----- |\n';
+            markdownTable += '| ------ | -- | -- | -- |\n';
             reportData.forEach(item => {
-                markdownTable += `| ${item.fileName} | ${item.coveredLine} | ${item.totalLine} | ${item.coverage}% |\n`;
+                markdownTable += `| <details><summary>${item.packageName}</summary>${item.fileName}</details> | ${item.coveredLine} | ${item.totalLine} | ${item.coverage}% |\n`;
             });
-            console.log(markdownTable);
-            fs.writeFileSync('report-table.md', markdownTable, 'utf8');
+            core.info(markdownTable);
+            const filePath = 'report-table.md';
+            fs.writeFileSync(filePath, markdownTable, 'utf8');
+            // 执行 Git 命令将文件添加到仓库并提交
+            (0, child_process_1.execSync)(`git add ${filePath}`);
+            (0, child_process_1.execSync)(`git commit -m "Add generated report table"`);
+            // 如果你想要推送到远程仓库，你需要配置远程仓库的 URL 并执行 push 命令
+            // execSync(`git remote add origin https://github.com/yourusername/yourrepo.git`); // 只需要执行一次来设置远程仓库
+            (0, child_process_1.execSync)(`git push origin main`); // 假设你的主分支名为 main
         }
         catch (error) {
             console.error('Error fetching report data:', error);
@@ -27583,7 +27592,7 @@ async function run() {
             reportPath: core.getInput("reportPath", { required: false }),
         };
         const theRunner = new runner.TestsRunner();
-        await theRunner.generateSummaryTable(runOptions, [{ fileName: "example.java", coveredLine: 223, totalLine: 251, coverage: 88 }]);
+        await theRunner.generateSummaryTable(runOptions, [{ fileName: "example.java", packageName: "com.example.package", coveredLine: 223, totalLine: 251, coverage: 88 }]);
     }
     catch (error) {
         if (error instanceof Error) {
