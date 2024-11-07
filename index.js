d484ea1bd7d6ce069bb5eecdc473e5bfc328071f
Jason <jchen@parasoft.com>
2024-11-07 15:14:15 +0800

    Change to js implement

---
+++ index.js
@@ -0,0 +1,9 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { action } = require("./src/action");
+
+action(github.context.payload).catch((error) => {
+  // Action threw an error. Fail the action with the error message.
+  core.debug(error.stack);
+  core.setFailed(error.message);
+});
