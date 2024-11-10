5905d02bf169b53c15bccef8b65f4ce70419c649
Jason <jchen@parasoft.com>
2024-11-11 02:48:04 +0800

    update code

--- dist/index.js
+++ dist/index.js
@@ -224,19 +224,43 @@ class TestsRunner {
                         '<td style="width: 400px;">' + Math.floor(report.total) + '%</td>\r\n' +
                         '  </tr>\r\n' +
                         '</table>\r\n';
-                report.files.forEach(file => {
+                report.files.forEach((file, index) => {
                     const fileTotal = Math.floor(file.total);
                     // const fileBranch = Math.floor(file.branch);
                     const className = this.escapeMarkdown(file.name);
-                    markdownTable += '<details>\r\n' +
-                        '<summary>' + className.substring(0, className.lastIndexOf(".")) + '</summary> \r\n' +
-                        '<table>\r\n' +
-                        '  <tr>\r\n' +
-                        '<td style="width: 400px;">' + className + '</td>\r\n' +
-                        '<td style="width: 400px;">' + fileTotal + '%</td>\r\n' +
-                        '  </tr>\r\n' +
-                        '</table>\r\n' +
-                        '</details>\r\n';
+                    if (index === 0) {
+                        markdownTable += '<details>\r\n' +
+                            '<summary>' + className.substring(0, className.lastIndexOf(".")) + '</summary> \r\n' +
+                            '<table>\r\n' +
+                            '  <tr>\r\n' +
+                            '    <th style="width: 400px;">File</th>\r\n' +
+                            '    <th style="width: 100px;">Coverage</th>\r\n' +
+                            '  </tr>\r\n' +
+                            '  <tr>\r\n' +
+                            '<td style="width: 400px;">' + className + '</td>\r\n' +
+                            '<td style="width: 400px;">' + fileTotal + '%</td>\r\n' +
+                            '  </tr>\r\n';
+                    }
+                    else {
+                        if (file.filename === preFileName) {
+                            markdownTable += '  <tr>\r\n' +
+                                '    <th style="width: 400px;">File</th>\r\n' +
+                                '    <th style="width: 100px;">Coverage</th>\r\n' +
+                                '  </tr>\r\n';
+                        }
+                        else {
+                            preFileName = file.filename;
+                            markdownTable += '</table>\r\n' +
+                                '</details>\r\n' +
+                                '<details>\r\n' +
+                                '<summary>' + className.substring(0, className.lastIndexOf(".")) + '</summary> \r\n' +
+                                '<table>\r\n' +
+                                '  <tr>\r\n' +
+                                '    <th style="width: 400px;">File</th>\r\n' +
+                                '    <th style="width: 100px;">Coverage</th>\r\n' +
+                                '  </tr>\r\n';
+                        }
+                    }
                 });
             });
             const pullRequest = github.context.payload.pull_request;
