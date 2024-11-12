5be2e13282d29266dba6e1b895243e790d08e97f
Jason <jchen@parasoft.com>
2024-11-13 01:32:32 +0800

    Optimize code

--- dist/index.js
+++ dist/index.js
@@ -9,24 +9,25 @@
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.generateWorkflowSummary = generateWorkflowSummary;
 const core = __nccwpck_require__(2186);
-function processPackages(packages) {
-    let markdown = '';
-    packages.forEach(packageCoverage => {
-        markdown += "<details><summary>" + packageCoverage.name + "&emsp;(80/100 - " + packageCoverage.lineRate + "%)</summary><table><tbody>";
-        packageCoverage.classes.forEach(classCoverage => {
-            markdown += "<tr><td>&emsp;" + classCoverage.fileName + "&emsp;(" + classCoverage.lines.length + "/100 - " + classCoverage.lineRate + "%)</td></tr>\n";
-        });
-        markdown += "</tbody></table><details>";
-    });
-    return markdown;
-}
 async function generateWorkflowSummary(coverage) {
-    const markdown = processPackages(coverage.packages);
+    const markdowns = processPackages(coverage.packages);
     return await core.summary
         .addTable([[{ data: "Coverage&emsp;(covered/total - percentage)", header: true }],
-        ["<b>Total coverage&emsp;(" + coverage.linesCovered + "/100 - " + coverage.lineRate + "%)</b>"],
-        [markdown]])
+        ["<b>Total coverage&emsp;(" + coverage.linesCovered + "/" + coverage.linesValid + " - " + coverage.lineRate * 100 + "%)</b>"],
+        markdowns])
         .write();
+}
+function processPackages(packages) {
+    const markdowns = [];
+    packages.forEach(packageCoverage => {
+        let markdown = "<details><summary>" + packageCoverage.name + "&emsp;(80/100 - " + packageCoverage.lineRate + "%)</summary><table><tbody>";
+        packageCoverage.classes.forEach(classCoverage => {
+            markdown += "<tr><td>&emsp;" + classCoverage.name + "&emsp;(" + classCoverage.lines.length + "/100 - " + classCoverage.lineRate + "%)</td></tr>\n";
+        });
+        markdown += "</tbody></table><details>";
+        markdowns.push(markdown);
+    });
+    return markdowns;
 }
 //# sourceMappingURL=action.js.map
 
--- src/action.ts
+++ src/action.ts
@@ -1,23 +1,24 @@
 import * as core from "@actions/core";
 import {CoberturaCoverage, CoberturaPackage} from "./report";
 
-function processPackages(packages: Map<string, CoberturaPackage>) {
-    let markdown = '';
-    packages.forEach(packageCoverage => {
-        markdown += "<details><summary>" + packageCoverage.name + "&emsp;(80/100 - "+ packageCoverage.lineRate + "%)</summary><table><tbody>";
-        packageCoverage.classes.forEach(classCoverage => {
-            markdown += "<tr><td>&emsp;" + classCoverage.fileName +"&emsp;(" + classCoverage.lines.length + "/100 - " + classCoverage.lineRate + "%)</td></tr>\n"
-        });
-        markdown += "</tbody></table><details>"
-    });
-    return markdown;
-}
-
 export async function generateWorkflowSummary(coverage: CoberturaCoverage) {
-    const markdown = processPackages(coverage.packages);
+    const markdowns = processPackages(coverage.packages);
     return await core.summary
         .addTable([[{data: "Coverage&emsp;(covered/total - percentage)", header: true}],
-            ["<b>Total coverage&emsp;(" + coverage.linesCovered + "/100 - " + coverage.lineRate + "%)</b>"],
-            [markdown]])
+            ["<b>Total coverage&emsp;(" + coverage.linesCovered + "/" + coverage.linesValid + " - " + coverage.lineRate*100 + "%)</b>"],
+            markdowns])
         .write();
+}
+
+function processPackages(packages: Map<string, CoberturaPackage>) {
+    const markdowns: string[] = [];
+    packages.forEach(packageCoverage => {
+        let markdown = "<details><summary>" + packageCoverage.name + "&emsp;(80/100 - "+ packageCoverage.lineRate + "%)</summary><table><tbody>";
+        packageCoverage.classes.forEach(classCoverage => {
+            markdown += "<tr><td>&emsp;" + classCoverage.name +"&emsp;(" + classCoverage.lines.length + "/100 - " + classCoverage.lineRate + "%)</td></tr>\n"
+        });
+        markdown += "</tbody></table><details>"
+        markdowns.push(markdown);
+    });
+    return markdowns;
 }
