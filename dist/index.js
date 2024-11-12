34f345993e573aa9abac21c30e5770c29230d9e6
Jason <jchen@parasoft.com>
2024-11-12 15:31:44 +0800

    Update code

--- dist/index.js
+++ dist/index.js
@@ -78,15 +78,14 @@ async function createCheckRun(repoToken, reportTable) {
 async function generateWorkflowSummary() {
     return await core.summary
         .addHeading('Coverage&emsp;(covered/total - percentage)', 5)
-        .addSeparator()
-        .addDetails("com.parasoft.package1.another", "&emsp;&emsp;com.parasoft.package1.another.Convertor (100/100)")
-        .addRaw("<hr>")
-        .addDetails("com.parasoft.package1&emsp;(80/100 - 80%)", "<ul class='aaa' style=\"list-style-type: none;\"><li>class1.java&emsp;(60/100 - 60%)\n" +
-        "<li>class2.java&emsp;(90/100 - 90%)</li><li><details><summary style=\"margin-left: 20px;\">com.parasoft.package1.subpackage1&emsp;(80/100 - 80%)</summary>\n" +
-        "<ul style=\"list-style-type: none;\"><li>class1.java&emsp;(60/100 - 60%)</li><li>class2.java&emsp;(90/100 - 90%)</li>\n" +
-        "<li>class2.java&emsp;(50/100 - 50%)</li></details></li></ul>")
-        .addRaw("<hr>")
-        .addDetails("com.parasoft.package2.another", "&emsp;&emsp;com.parasoft.package2.another.Convertor (100/100)")
+        .addTable([["<details><summary>com.parasoft.package1.another</summary>&emsp;&emsp;com.parasoft.package1.another.Convertor (100/100)</details>"]])
+        .addEOL()
+        .addDetails("com.parasoft.package1&emsp;(80/100 - 80%)", "<table><tr><td>class1.java&emsp;(60/100 - 60%)</td>\n" +
+        "<td>class2.java&emsp;(90/100 - 90%)</td><td><details><summary style=\"margin-left: 20px;\">com.parasoft.package1.subpackage1&emsp;(80/100 - 80%)</summary>\n" +
+        "<table style=\"list-style-type: none;\"><tbody><tr><td>class1.java&emsp;(60/100 - 60%)</td><td>class2.java&emsp;(90/100 - 90%)</td>\n" +
+        "<td>class2.java&emsp;(50/100 - 50%)</td></tr></tbody></table></details></td></tr></table>")
+        .addEOL()
+        .addTable([["<details><summary>com.parasoft.package2.another</summary>&emsp;&emsp;com.parasoft.package2.another.Convertor (100/100)</details>"]])
         .write();
 }
 // async function generateChecks (repoToken, reportTable) {
