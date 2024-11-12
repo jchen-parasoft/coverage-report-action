39ae7d14c1c19c6bc30281bd08969ff4a8dbd38e
Jason <jchen@parasoft.com>
2024-11-12 09:42:11 +0800

    Optimize code

---
+++ src/action.ts
@@ -0,0 +1,111 @@
+import * as core from "@actions/core";
+import * as github from '@actions/github';
+import {Endpoints} from '@octokit/types/dist-types/generated/Endpoints';
+import {ProcessCoverageResult} from "./report";
+
+const pullRequest = github.context.payload.pull_request;
+const link = (pullRequest && pullRequest.html_url) || github.context.ref;
+const headSha = (pullRequest && pullRequest.head.sha) || github.context.sha;
+let totalCoverage = 0;
+
+export function generateReportTable(reports: ProcessCoverageResult[]) {
+    try {
+        const tableTitleMarkdown = '<tr>\r\n' + '<th>File</th>\r\n' + '<th>Coverage</th>\r\n' + '</tr>\r\n'
+        let preFileName = '';
+        let reportTableMarkdown = '';
+
+        reports.forEach(report => {
+            totalCoverage = Math.floor(report.total);
+            reportTableMarkdown +=
+                '<table>\r\n' + tableTitleMarkdown + tableContentMarkdown('All files', totalCoverage) + '</table>\r\n';
+
+            report.files.forEach((file, index) => {
+                const fileTotal = Math.floor(file.total);
+                // const fileBranch = Math.floor(file.branch);
+                const className = file.name;
+
+                if (index === 0) {
+                    reportTableMarkdown +=
+                        '<details>\r\n' + '<summary>' + className.substring(0, className.lastIndexOf(".")) + '</summary> \r\n' +
+                        '<table>\r\n' + tableTitleMarkdown + tableContentMarkdown(className, fileTotal);
+                } else {
+                    if (file.filename === preFileName) {
+                        reportTableMarkdown += tableContentMarkdown(className, fileTotal);
+                    } else {
+                        preFileName = file.filename;
+                        reportTableMarkdown +=
+                            '</table>\r\n' + '</details>\r\n' +
+                            '<details>\r\n' + '<summary>' + className.substring(0, className.lastIndexOf(".")) + '</summary> \r\n' +
+                            '<table>\r\n' + tableTitleMarkdown + tableContentMarkdown(className, fileTotal);
+                    }
+                }
+            });
+        });
+        return reportTableMarkdown;
+    } catch (error) {
+        console.error('Error fetching report data:', error);
+    }
+}
+
+function tableContentMarkdown(fileName:string, coverage: number) {
+    return '<tr>\r\n' + '<td>' + fileName + '</td>\r\n' + '<td>' + coverage + '%</td>\r\n' + '</tr>\r\n';
+}
+
+export async function createCheckRun(repoToken: string, reportTable: string | undefined) {
+    const checkName = github.context.runId + " coverage";
+    const client = github.getOctokit(repoToken);
+    const createCheckRequest = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        name: checkName,
+        head_sha: headSha,
+        status: "completed",
+        conclusion: "success",
+        output: {
+            title: "Results",
+            summary: reportTable
+        }
+    } as Endpoints['POST /repos/{owner}/{repo}/check-runs']['parameters']
+    core.info(
+        `Posting status 'completed' with conclusion 'success' to ${link} (sha: ${headSha})`
+    );
+    return await client.rest.checks.create(createCheckRequest);
+}
+
+export async function generateWorkflowSummary(createCheckRunResponse) {
+    let checkRunHtmlUrl = '';
+    if(createCheckRunResponse.data.html_url != null) {
+        checkRunHtmlUrl = createCheckRunResponse.data.html_url;
+    }
+
+    return await core.summary
+        .addHeading('Test Results')
+        .addTable([
+            [{data: 'File', header: true}, {data: 'Result', header: true}],
+            ['All files', totalCoverage + "%"]
+        ])
+        .addRaw("For more details, see ")
+        .addLink('this check', checkRunHtmlUrl)
+        .addBreak()
+        .addBreak()
+        .addRaw("Results for commit ")
+        .addLink(headSha.substring(0,7), github.context.payload.repository?.html_url + "/commit/" + headSha)
+        .write();
+}
+
+// async function generateChecks (repoToken, reportTable) {
+//     const checkName = github.context.runId + " coverage";
+//     const client = github.getOctokit(repoToken);
+//     const listCheckResponse = listCheckFromRepo(client);
+//     const createCheckResponse = createCheck(repoToken, reportTable);
+// const index = listForRefResponse.data.check_runs.length;
+// }
+
+// async function listCheckFromRepo(client) {
+//     const listCheckRequest = {
+//         owner: github.context.repo.owner,
+//         repo: github.context.repo.repo,
+//         ref: headSha
+//     } as Endpoints['GET /repos/{owner}/{repo}/commits/{ref}/check-runs']['parameters']
+//     return await client.rest.checks.listForRef(listCheckRequest);
+// }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,15 @@
 import * as core from "@actions/core";
-import * as runner from './runner';
-import * as cobertura from './cobertura';
-
-const skipCovered = JSON.parse(
-    core.getInput("skip_covered", { required: true }),
-);
+import * as action from './action';
+import * as report from './report';
 
 export async function run() {
     try {
-        const runOptions: runner.RunOptions = {
-            repoToken: core.getInput("repo_token", { required: true })
-        };
-
-        const reports = await cobertura.processCoverage(core.getInput("path", { required: true }), skipCovered);
-        const theRunner = new runner.TestsRunner();
-        await theRunner.generateSummaryTable(runOptions, reports);
+        const skipCovered = JSON.parse(core.getInput("skip_covered", { required: true }));
+        const repoToken = core.getInput("repo_token", { required: true });
+        const reports = await report.processCoverage(core.getInput("path", { required: true }), skipCovered);
+        const reportTableMarkdown = action.generateReportTable(reports);
+        const createCheckRunResponse = await action.createCheckRun(repoToken, reportTableMarkdown);
+        await action.generateWorkflowSummary(createCheckRunResponse);
     } catch (error) {
         if (error instanceof Error) {
             core.error(error);
