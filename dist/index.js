702b78121da912f85970d8d9c40390e077bda149
Jason <jchen@parasoft.com>
2024-11-13 09:47:36 +0800

    Update code

--- dist/index.js
+++ dist/index.js
@@ -10,21 +10,23 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.generateWorkflowSummary = generateWorkflowSummary;
 const core = __nccwpck_require__(2186);
 async function generateWorkflowSummary(coverage) {
-    const summary = core.summary
+    const markdown = processPackages(coverage.packages);
+    return await core.summary
         .addTable([[{ data: "Coverage&emsp;(covered/total - percentage)", header: true }],
-        ["<b>Total coverage&emsp;(" + coverage.linesCovered + "/" + coverage.linesValid + " - " + Math.floor(coverage.lineRate) + "%)</b>"]]);
-    return processPackages(coverage.packages, summary).write();
+        ["<b>Total coverage&emsp;(" + coverage.linesCovered + "/" + coverage.linesValid + " - " + Math.floor(coverage.lineRate * 100) + "%)</b>"]])
+        .addRaw(markdown)
+        .write();
 }
-function processPackages(packages, summary) {
+function processPackages(packages) {
+    let markdown = '';
     packages.forEach(packageCoverage => {
-        let markdown = "<details><summary>" + packageCoverage.name + "&emsp;(80/100 - " + Math.floor(packageCoverage.lineRate) + "%)</summary><table><tbody>";
+        markdown += "<table><details><summary>" + packageCoverage.name + "&emsp;(80/100 - " + Math.floor(packageCoverage.lineRate * 100) + "%)</summary><table><tbody>";
         packageCoverage.classes.forEach(classCoverage => {
-            markdown += "<tr><td>&emsp;" + classCoverage.name + "&emsp;(" + classCoverage.lines.length + "/100 - " + Math.floor(classCoverage.lineRate) + "%)</td></tr>\n";
+            markdown += "<tr><td>&emsp;" + classCoverage.name + "&emsp;(" + classCoverage.lines.length + "/100 - " + Math.floor(classCoverage.lineRate * 100) + "%)</td></tr>\n";
         });
-        markdown += "</tbody></table></details>";
-        summary.addTable([[markdown]]);
+        markdown += "</tbody></table></details></table>";
     });
-    return summary;
+    return markdown;
 }
 //# sourceMappingURL=action.js.map
 
